079a387b637e5e18805e3e72982bde7cc72bd00e
rytakagi <tkgriot@outlook.jp>
2015-12-02 01:58:36 +0900

    151202

--- src/Arana.php
+++ src/Arana.php
@@ -4,12 +4,13 @@ namespace Antron\Arana;
 
 class Arana
 {
-    private static $config=[
-        'delimiter'=>',',
-        'header'=>true,
-        'encode'=>'UTF-8',
-        'quotation'=>true,
-        'kana'=>false,
+
+    private static $config = [
+        'delimiter' => ',',
+        'header' => true,
+        'encode' => 'UTF-8',
+        'quotation' => true,
+        'kana' => false,
     ];
 
     public static function readCsv($config_arg)
@@ -35,7 +36,7 @@ class Arana
         }
     }
 
-    public static function readTxt($filepath, $encode, $kana = false)
+    public static function readTxt($filepath, $encode = '', $kana = false)
     {
         $texts = [];
 
@@ -62,16 +63,24 @@ class Arana
         return $texts;
     }
 
-    public static function write($arrays, $config_arg=[])
+    public static function write($arrays, $config_arg = [])
     {
+        if (!$arrays) {
+            return;
+        }
+
         $string_implode = '';
 
         $config = self::config($config_arg);
 
         if ($config['header']) {
-            $array_tmp = array_shift($arrays);
+            $headers = [];
+            foreach ($arrays[0] as $key => $value) {
+                $headers[] = $key;
+                $value = null;
+            }
 
-            $string_implode.=implode($config['delimiter'], array_flip($array_tmp)) . "\n";
+            $string_implode.=implode($config['delimiter'], $headers) . "\n";
         }
 
         foreach ($arrays as $array) {
@@ -88,18 +97,18 @@ class Arana
         if (!isset($config['filepath'])) {
             $config['filepath'] = storage_path('arana.txt');
         }
-        
+
         return $config + self::$config;
     }
 
     private static function toHash($csv)
     {
         $hash = [];
-        
-        if(!$csv){
+
+        if (!$csv) {
             return $hash;
         }
-        
+
         foreach (array_shift($csv) as $key => $value) {
             $heads[$key] = $value;
         }
